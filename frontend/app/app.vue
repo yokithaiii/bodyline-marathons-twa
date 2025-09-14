7bdc840ace30742c3826c437b12486937299b3f7
nastyonai <nirvanastasia@gmail.com>
2025-09-14 14:28:30 +0300

    fix: testing

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -7,6 +7,11 @@ onBeforeMount(() => {
 </script>
 
 <template>
-	test page
-	
+	<UApp>
+		<div class="bg-[var(--ui-bg)]" vaul-drawer-wrapper>
+			<NuxtLayout>
+				<NuxtPage />
+			</NuxtLayout>
+		</div>
+	</UApp>
 </template>
